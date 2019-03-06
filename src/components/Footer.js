7378967b67c380b6c0964c6743acdc9d37df9e3b
i-shubhamprakash <shubham.prakash2308@gmail.com>
2019-03-06 22:13:35 +0530

    Complete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import '../styles/Footer.css'
+import SocialLink from './ComponentBox'
 const Footer= (props)=>{
     return(
         <div className="footer-container">
             <div className="footer-top container-fluid">
                 <div className="row">
-                    <div className="col col-sm-12 col-lg-4">lala</div>
-                    <div className="col col-sm-12 col-lg-4">baba</div>
-                    <div className="col col-sm-12 col-lg-4">kaka</div>
+                    <div className="col col-12 col-lg-4">
+                    <p>HALDIA INSTITUTE OF TECHNOLOGY<br/>
+                    Haldia, 721657 Tel: +91 3224 252800 <br/>
+                    contactus@ithit.com</p>
+                    </div>
+                    <div className="col col-12 col-lg-4"></div>
+                    <div className="col col-12 col-lg-4">
+                        <SocialLink
+                        facebook="https://fb.com"
+                        twitter="https://twitter.com"
+                        linkedin="https://linkedin.com"
+                        />
+                    </div>
                 </div>
             </div>
-            <div className="footer-bottom">All right reserved @ IT-HIT.in</div>
+            <div className="footer-bottom">© 2018 Copyright ithit.com</div>
         </div>
     );
 }
