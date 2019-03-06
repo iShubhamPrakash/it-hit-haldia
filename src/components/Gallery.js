f7600ab3010e3ee4e7267ef3c8fd1977b4295587
i-shubhamprakash <shubham.prakash2308@gmail.com>
2019-03-07 02:31:24 +0530

    Add gallery component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,29 +3,39 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import '../styles/Gallery.css'
 import img1 from '../assets/hit1.jpg'
+import img2 from '../assets/hit2.jpg'
+import img3 from '../assets/hit3.jpg'
+import img4 from '../assets/hit4.jpg'
+import img5 from '../assets/hit5.jpg'
 
 export default class Gallery extends Component {
   render() {
     return (
       <div className="gallery-container">
         <div className="gallery-header">
-            
+            <h1 className="text-center">Gallery</h1>
         </div>
-    
-        <Carousel>
+        <div className="gallery-body">
+          <div className="galery-carousel">
+          <Carousel>
             <div>
                 <img src={img1} />
-                <p className="legend">Legend 1</p>
             </div>
             <div>
-                <img src={img1} />
-                <p className="legend">Legend 2</p>
+                <img src={img2} />
             </div>
             <div>
-                <img src={img1} />
-                <p className="legend">Legend 3</p>
+                <img src={img3} />
+            </div>
+            <div>
+                <img src={img4} />
+            </div>
+            <div>
+                <img src={img5} />
             </div>
         </Carousel>
+          </div>
+        </div>
     </div>  
     )
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,10 +7,11 @@ const Nav= (props)=>{
     return(
         <div className="nav-container">
             <ul>
-                <li><NavLink exact to="/"><img src={logo} alt="" className="nav_logo"/> Information Technology</NavLink></li>
+                <li><img src={logo} alt="" className="nav_logo"/> Information Technology</li>
                 <li><NavLink exact to="/">Home</NavLink></li>
-                <li><NavLink exact to="/about-department">AboutDepartment</NavLink></li>
-                <li><NavLink exact to="/about-college">AboutCollege</NavLink></li>
+                <li><NavLink exact to="/about-department">About Department</NavLink></li>
+                <li><NavLink exact to="/about-college">About College</NavLink></li>
+                <li><NavLink exact to="/gallery">Gallery</NavLink></li>
                 <li><NavLink exact to="/contact">Contact us</NavLink></li>
                 <li className="nav-spacer"></li>
                 <li><button className="sign-in-btn"><i className="fa fa-sign-in-alt"/> Sign In</button></li>
