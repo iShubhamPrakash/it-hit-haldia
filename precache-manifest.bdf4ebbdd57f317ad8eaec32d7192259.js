2327821687f18ec15497ff773f16d93efc06d7d1
i-shubhamprakash <shubham.prakash2308@gmail.com>
2019-03-07 02:41:31 +0530

    Updates

---
+++ precache-manifest.bdf4ebbdd57f317ad8eaec32d7192259.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "28191f7bfc1fa75629f96005e930bf5f",
+    "url": "/information.technology.HIT.haldia/static/media/it_logo.28191f7b.png"
+  },
+  {
+    "revision": "e757de5adba2934835e0",
+    "url": "/information.technology.HIT.haldia/static/css/main.5e72a6fb.chunk.css"
+  },
+  {
+    "revision": "e5963c019b9425e3adc9",
+    "url": "/information.technology.HIT.haldia/static/js/runtime~main.e5963c01.js"
+  },
+  {
+    "revision": "739792cb54e8084704eb73c353ab9730",
+    "url": "/information.technology.HIT.haldia/static/media/hit5.739792cb.jpg"
+  },
+  {
+    "revision": "be48d9c46f4800b3cd8b",
+    "url": "/information.technology.HIT.haldia/static/js/2.be48d9c4.chunk.js"
+  },
+  {
+    "revision": "ed4218e87c1ac08ceda9ad85ba448a05",
+    "url": "/information.technology.HIT.haldia/static/media/hit-home-l.ed4218e8.jpg"
+  },
+  {
+    "revision": "3d29e454606a13b51e752936b4aa4821",
+    "url": "/information.technology.HIT.haldia/static/media/hit_logo.3d29e454.png"
+  },
+  {
+    "revision": "e757de5adba2934835e0",
+    "url": "/information.technology.HIT.haldia/static/js/main.e757de5a.chunk.js"
+  },
+  {
+    "revision": "e1ea5866e207086864f6e0dbed9139fb",
+    "url": "/information.technology.HIT.haldia/static/media/hit1.e1ea5866.jpg"
+  },
+  {
+    "revision": "634ea1f5879652e52e8b7a3184c87b12",
+    "url": "/information.technology.HIT.haldia/static/media/hit2.634ea1f5.jpg"
+  },
+  {
+    "revision": "c10f3a15a8379b8412d6ac906cd1f493",
+    "url": "/information.technology.HIT.haldia/static/media/hit3.c10f3a15.jpg"
+  },
+  {
+    "revision": "cce5b73817bdc99e185eec05d9b6f08f",
+    "url": "/information.technology.HIT.haldia/static/media/hit4.cce5b738.jpg"
+  },
+  {
+    "revision": "be48d9c46f4800b3cd8b",
+    "url": "/information.technology.HIT.haldia/static/css/2.ccc9914d.chunk.css"
+  },
+  {
+    "revision": "299a2bad8d9e3762fdbe6684022ced9a",
+    "url": "/information.technology.HIT.haldia/index.html"
+  }
+];
